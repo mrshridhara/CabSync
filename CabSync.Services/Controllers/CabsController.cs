97c708d69c92791651173648621fb9d37238bfdc
Shridhara Rangaraju <mrshridhara@outlook.com>
2015-05-17 11:10:58 +0530

    Added first api controller

---
+++ CabSync.Services/Controllers/CabsController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace CabSync.Services.Controllers
+{
+    public class CabsController : ApiController
+    {
+        public IHttpActionResult Get() => Ok(new { Message = $"Reached get method in {nameof(CabsController)}" });
+    }
+}
